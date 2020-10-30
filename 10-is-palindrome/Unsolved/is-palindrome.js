12e52021ad08ed01c311d99f8aa12a097d6c24a0
Jonghyun Kang <misterjaykay@gmail.com>
2020-10-30 16:54:21 -0400

    solved

--- 10-is-palindrome/Unsolved/is-palindrome.js
+++ 10-is-palindrome/Unsolved/is-palindrome.js
@@ -1,3 +1,21 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
-var isPalindrome = function(str) {};
+var isPalindrome = function(str) {
+    var testStr = "";
+    var result;
+    for (let i = 0; i < str.length; i++) {
+        const element = str[i].toLowerCase();
+        const opposite = str[str.length - i - 1].toLowerCase();
+        testStr += opposite;
+    }
+    if(testStr === str) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+
+var str = "testing";
+isPalindrome(str);
+
