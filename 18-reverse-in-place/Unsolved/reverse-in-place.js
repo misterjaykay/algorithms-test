bef727d30f977564ec9d3e1f23f8b96f94f00734
Jonghyun Kang <misterjaykay@gmail.com>
2020-11-02 13:37:29 -0500

    solved

--- 18-reverse-in-place/Unsolved/reverse-in-place.js
+++ 18-reverse-in-place/Unsolved/reverse-in-place.js
@@ -1,4 +1,19 @@
 // Write code to return a new string with all of the characters in `str` reversed.
 // You may NOT use the built-in reverse method
 
-var reverseInPlace = function(arr) {};
+var reverseInPlace = function(arr) {
+    var newArr = [];
+    // var str = arr.split("");
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        console.log(element);
+        newArr.unshift(element);
+        console.log(newArr);
+        
+    }
+    return newArr;
+};
+
+var arr = [2, 4, 6, 8];
+
+reverseInPlace(arr);
