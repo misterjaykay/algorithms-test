7aaea4b34d9fe13d3f91a8433571ab1c3548f67b
Jonghyun Kang <misterjaykay@gmail.com>
2020-11-04 23:37:13 -0500

    solved

--- 19-look-say-sequence/Unsolved/look-and-say-sequence.js
+++ 19-look-say-sequence/Unsolved/look-and-say-sequence.js
@@ -1,5 +1,32 @@
 // Write code to create a function that accepts a number (`n`) and returns a new number that satisfies the look-and-say-sequence
 
 var lookAndSay = function(n) {
+    var arr = n.toString();
+    var res = "";
 
+    var count = 1;
+    var current = arr[0];
+
+    for (let i = 1; i <= arr.length; i++) {
+        const element = arr[i];
+
+        console.log("check", current);
+        console.log("loop", element);
+
+        if (element === current) {
+            count++;
+            console.log(element, current);
+        } else {
+            res += count;
+            res += current;
+            count = 1;
+            current = element;
+        }
+    }
+    console.log(parseInt(res));
+    return parseInt(res);
 };
+
+var n = 335561;
+
+lookAndSay(n);
--- 20-is-numeric/Unsolved/is-numeric.js
+++ 20-is-numeric/Unsolved/is-numeric.js
@@ -1,3 +1,51 @@
 // Write code to create a function that accepts a string and determines if the string is a valid number. You may not use any regex, built-in methods type conversion to accomplish this.
 
-var isNumeric = function(str) {};
+var isNumeric = function(str) {
+    var numb = [ "1", "2", "3", "4", "5", "6", "7", "8", "9" ,"0" ];
+
+    var hasNum = false;
+    var hasDecimal = false;
+
+    for (let i = 0; i < str.length; i++) {
+        const e = str[i];
+
+        if (numb.includes(e)) {
+            hasNum = true;
+            continue;
+        } 
+
+        if (i === 0) {
+            if (e === "-" || e ==="+") {
+                continue;
+            }
+        }
+
+        if (e === ".") {
+            if (hasDecimal === true) {
+                return false;
+            }
+            hasDecimal = true;
+            continue;
+        }
+
+        return false;
+    }
+
+    if (hasNum) {
+        return true;
+    }
+
+    return false;
+    // if (Number.isInteger(numb)) {
+    //     console.log(true);
+    //     return true;
+    // } else {
+    //     console.log(false);
+    //     return false;
+    // }
+};
+
+// var str = "-0123.00";
+var str = "55-";
+
+isNumeric(str);
